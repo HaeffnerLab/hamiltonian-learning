09e06e9df87f243215992cfa2fd48089bc9f2331
Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
2022-01-07 08:45:04 -0500

    Improve AC Stark shift implementation

--- ms_calibration.jl
+++ ms_calibration.jl
@@ -6,11 +6,19 @@ using Random
 using Optim
 using Distributions
 
+# hard-coded physical parameters of the experiment
+magnetic_field_gauss = 4.0
+real_sym_ac_stark_shift_khz = 1.0
+real_antisym_ac_stark_shift_khz = -0.035
+ν_ax_mhz = 1.0
+ν_rad_mhz = 3.0
+
 struct HamiltonianParameters
     detuning_khz::Float64
     pi_time_blue_μs::Float64
     pi_time_red_μs::Float64
-    ac_stark_shift_hz::Float64
+    sym_ac_stark_shift_khz::Float64
+    antisym_ac_stark_shift_khz::Float64
     HamiltonianParameters(params::Vector) = new(params...)
 end
 
@@ -18,30 +26,41 @@ function as_vector(θ::HamiltonianParameters)
     return [getproperty(θ, Symbol(name)) for name in fieldnames(HamiltonianParameters)]
 end
 
-function simulate_trap(tspan, θ::HamiltonianParameters)
-    # defining trap parameters
-    ca_ions = [Ca40(["S-1/2", "D-1/2"]), Ca40(["S-1/2", "D-1/2"])]
+function configure_trap(ions, δB=0)
+    for ion in ions
+        ion.stark_shift["S-1/2"] = real_sym_ac_stark_shift_khz * 1e3
+    end
     chain = LinearChain(
-        ions=ca_ions,
-        com_frequencies=(x=3e6,y=3e6,z=1e6), 
+        ions=ions,
+        com_frequencies=(x=ν_rad_mhz*1e6, y=ν_rad_mhz*1e6, z=ν_ax_mhz*1e6), 
         vibrational_modes=(;z=[1]),
     )
     lasers = [
         Laser(k = (x̂ + ẑ)/√2, ϵ = (x̂ - ẑ)/√2),
         Laser(k = (x̂ + ẑ)/√2, ϵ = (x̂ - ẑ)/√2)
     ]
-    trap = Trap(configuration=chain, B=4e-4, Bhat=ẑ, δB=0, lasers=lasers)
+    trap = Trap(configuration=chain, B=magnetic_field_gauss*1e-4, Bhat=ẑ, δB=δB, lasers=lasers)
 
     mode = trap.configuration.vibrational_modes.z[1]
     mode.N = 10
-    
-    Δ = θ.detuning_khz * 1e3
-    d = θ.ac_stark_shift_hz  # AC Stark shift compensation
+    η = abs(get_η(mode, lasers[1], ions[1]))
+
+    return trap, η
+end
+
+function simulate_trap(tspan, θ::HamiltonianParameters)
+    ca_ions = [Ca40(), Ca40()]
+    trap, _ = configure_trap(ca_ions)
+
     f = transition_frequency(trap, 1, ("S-1/2", "D-1/2"))
+    Δ = θ.detuning_khz * 1e3    
+    δ_blue = (θ.sym_ac_stark_shift_khz + θ.antisym_ac_stark_shift_khz) * 1e3
+    δ_red = (θ.sym_ac_stark_shift_khz - θ.antisym_ac_stark_shift_khz) * 1e3
     
-    detuned_lasers = [copy(lasers[1]), copy(lasers[2])]
-    detuned_lasers[1].Δ = f + mode.ν + Δ - d
-    detuned_lasers[2].Δ = f - mode.ν - Δ + d
+    mode = trap.configuration.vibrational_modes.z[1]
+    detuned_lasers = [copy(trap.lasers[1]), copy(trap.lasers[2])]
+    detuned_lasers[1].Δ = f + mode.ν + Δ + δ_blue
+    detuned_lasers[2].Δ = f - mode.ν - Δ + δ_red
     
     trap.lasers = detuned_lasers
     
@@ -53,17 +72,20 @@ function simulate_trap(tspan, θ::HamiltonianParameters)
         tspan, ca_ions[1]["S-1/2"] ⊗ ca_ions[2]["S-1/2"] ⊗ mode[0],
         h)
     
-    SS = real.(expect(ionprojector(trap, "S-1/2", "S-1/2"), sol))
-    DD = real.(expect(ionprojector(trap, "D-1/2", "D-1/2"), sol))
-    SD = real.(expect(ionprojector(trap, "S-1/2", "D-1/2"), sol))
-    DS = real.(expect(ionprojector(trap, "D-1/2", "S-1/2"), sol))
+    p_SS = real.(expect(ionprojector(trap, "S-1/2", "S-1/2"), sol))
+    p_DD = real.(expect(ionprojector(trap, "D-1/2", "D-1/2"), sol))
+    p_SD = real.(expect(ionprojector(trap, "S-1/2", "D-1/2"), sol))
+    p_DS = real.(expect(ionprojector(trap, "D-1/2", "S-1/2"), sol))
     
-    return tout, SS, DD, SD, DS
-end;
+    bell_state = dm((ca_ions[1]["S-1/2"] ⊗ ca_ions[2]["S-1/2"] + 1im * ca_ions[1]["D-1/2"] ⊗ ca_ions[2]["D-1/2"])/√2) ⊗ one(mode)
+    p_bell = real.(expect(bell_state, sol))
+    
+    return p_SS, p_DD, p_SD, p_DS, p_bell, tout
+end
 
 function log_likelihood(samples, num_experiments, times, θ::HamiltonianParameters)
     out = 0    
-    tout, p_SS, p_DD, p_SD, p_DS = simulate_trap(times, θ)
+    p_SS, p_DD, p_SD, p_DS, _ = simulate_trap(times, θ)
     for i in eachindex(times)
         probabilities = [p_SS[i], p_DD[i], p_SD[i], p_DS[i]] ./ (p_SS[i] + p_DD[i] + p_SD[i] + p_DS[i])
         dist = Multinomial(num_experiments, probabilities)
@@ -104,10 +126,10 @@ function ms_calibration(
     tspan_ideal,
     tspan_experiment
 )
-    tout, ideal_SS, ideal_DD, ideal_SD, ideal_DS = simulate_trap(tspan_ideal, θ_actual);
+    ideal_SS, ideal_DD, ideal_SD, ideal_DS, _ = simulate_trap(tspan_ideal, θ_actual);
 
     # run the experiment, learn the model, and reconstruct the curve from the learned model
-    tout, exp_SS, exp_DD, exp_SD, exp_DS = simulate_trap(tspan_experiment, θ_actual)
+    exp_SS, exp_DD, exp_SD, exp_DS, _, tout = simulate_trap(tspan_experiment, θ_actual)
     samples = simulate_experiment(N, exp_SS, exp_DD, exp_SD, exp_DS)
 
     function objective(θ::Vector)
@@ -118,7 +140,7 @@ function ms_calibration(
     θ_learned = Optim.minimizer(res)
     θ_learned = HamiltonianParameters(θ_learned)
 
-    tout, pred_SS, pred_DD, pred_SD, pred_DS = simulate_trap(tspan_ideal, θ_learned)
+    pred_SS, pred_DD, pred_SD, pred_DS, _ = simulate_trap(tspan_ideal, θ_learned)
     
     return Dict(
         "ideal_curve" => [
@@ -157,55 +179,34 @@ function ms_fidelity(θ_actual, θ_learned)
     adjust the frequency to match the learned AC stark shift,
     and then run the MS gate using a gate time of 1 / Δ.
     """
-    ca_ions = [Ca40(["S-1/2", "D-1/2"]), Ca40(["S-1/2", "D-1/2"])]
-    chain = LinearChain(
-        ions=ca_ions,
-        com_frequencies=(x=3e6,y=3e6,z=1e6), 
-        vibrational_modes=(;z=[1]),
-    )
-    lasers = [
-        Laser(k = (x̂ + ẑ)/√2, ϵ = (x̂ - ẑ)/√2),
-        Laser(k = (x̂ + ẑ)/√2, ϵ = (x̂ - ẑ)/√2)
-    ]
-    trap = Trap(configuration=chain, B=4e-4, Bhat=ẑ, δB=0, lasers=lasers)
-
-    mode = trap.configuration.vibrational_modes.z[1]
-    mode.N = 10
-    η = abs(get_η(mode, lasers[1], ca_ions[1]))
-
     # the physical laser frequency will be the actual detuning
-    Δ = θ_actual.detuning_khz * 1e3
+    physical_detuning_khz = θ_actual.detuning_khz
 
-    d = θ_learned.ac_stark_shift_hz  # AC Stark shift compensation
-    f = transition_frequency(trap, 1, ("S-1/2", "D-1/2"))
-    
-    detuned_lasers = [copy(lasers[1]), copy(lasers[2])]
-    detuned_lasers[1].Δ = f + mode.ν + Δ - d
-    detuned_lasers[2].Δ = f - mode.ν - Δ + d
-    
-    trap.lasers = detuned_lasers
-    
-    # calculate the desired experimental pi time using the learned detuning
-    desired_pi_time_μs = 1e6 * η / (θ_learned.detuning_khz * 1e3)
-    
-    # the resulting physical pi time will then be this desired pi time
+    # calculate the desired experimental pi time using the learned detuning,
+    # and the resulting physical pi time will then be this desired pi time
     # adjusted by the ratio of actual/learned pi time
-    physical_pi_time_blue_μs = desired_pi_time_μs * (θ_actual.pi_time_blue_μs/θ_learned.pi_time_blue_μs)
-    Efield_from_pi_time!(1e-6 * physical_pi_time_blue_μs, trap, 1, 1, ("S-1/2", "D-1/2"));
-    
+    _, η = configure_trap([Ca40(), Ca40()])
+    desired_pi_time_μs = 1e6 * η / (θ_learned.detuning_khz * 1e3)
+    physical_pi_time_blue_μs = desired_pi_time_μs * (θ_actual.pi_time_blue_μs/θ_learned.pi_time_blue_μs)    
     physical_pi_time_red_μs = desired_pi_time_μs * (θ_actual.pi_time_red_μs/θ_learned.pi_time_red_μs)
-    Efield_from_pi_time!(1e-6 * physical_pi_time_red_μs, trap, 2, 1, ("S-1/2", "D-1/2"));
-    
+
+    # use the learned AC Stark shift values
+    sym_ac_stark_shift_khz = θ_learned.sym_ac_stark_shift_khz
+    antisym_ac_stark_shift_khz = θ_learned.antisym_ac_stark_shift_khz
+
+    θ_calibrated = HamiltonianParameters([
+        physical_detuning_khz,
+        physical_pi_time_blue_μs,
+        physical_pi_time_red_μs,
+        sym_ac_stark_shift_khz,
+        antisym_ac_stark_shift_khz,
+    ])
+
     # evolve for the learned gate time
     learned_gate_time_μs = 1e6 / (θ_learned.detuning_khz * 1e3)
     tspan = LinRange(0, learned_gate_time_μs, 50)
-    h = hamiltonian(trap, timescale=1e-6, rwa_cutoff=1e5)
-    tout, sol = timeevolution.schroedinger_dynamic(
-        tspan, ca_ions[1]["S-1/2"] ⊗ ca_ions[2]["S-1/2"] ⊗ mode[0],
-        h)
+    _, _, _, _, p_bell, _ = simulate_trap(tspan, θ_calibrated)
 
-    # MS fidelity is overlap with desired Bell state
-    bell_state = dm((ca_ions[1]["S-1/2"] ⊗ ca_ions[2]["S-1/2"] + 1im * ca_ions[1]["D-1/2"] ⊗ ca_ions[2]["D-1/2"])/√2) ⊗ one(mode)
-    prob_bell = real.(expect(bell_state, sol))
-    return prob_bell[end]
+    # MS fidelity is overlap with desired Bell state at final time
+    return p_bell[end]
 end
